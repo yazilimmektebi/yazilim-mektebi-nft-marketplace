3ade8d136b6888b13d8d00a24ccacc66a3dc3d81
Yazilim Mektebi <103782896+yazilimmektebi@users.noreply.github.com>
2022-06-03 01:00:54 +0300

    initial

---
+++ collections.js
@@ -0,0 +1,182 @@
+let COLLECTIONS = [
+  {
+    id: 1,
+    name: "METABILLIONAIRE #2120",
+    author: "METABILLIONAIRE",
+    price: "0.27",
+    link:
+      "https://opensea.io/assets/ethereum/0xc6c817cd60e17fed0af2a759624e02dd6c812e64/2120",
+    img: "./images/1.png",
+    category: "collectibles",
+  },
+  {
+    id: 2,
+    name: "WD #5099",
+    author: "Whoopsies Doopsies",
+    price: "0.1",
+    link:
+      "https://opensea.io/assets/ethereum/0x565abc3feaa3bc3820b83620f4bbf16b5c4d47a3/5099",
+    img: "./images/2.png",
+    category: "collectibles",
+  },
+  {
+    id: 3,
+    name: "METABILLIONAIRE #5992",
+    author: "METABILLIONAIRE",
+    price: "0.4",
+    link:
+      "https://opensea.io/assets/ethereum/0xc6c817cd60e17fed0af2a759624e02dd6c812e64/5992",
+    img: "./images/3.png",
+    category: "collectibles",
+  },
+  {
+    id: 4,
+    name: "Maverick Mild",
+    author: "CyberBrokers",
+    price: "1.953",
+    link:
+      "https://opensea.io/assets/ethereum/0x565abc3feaa3bc3820b83620f4bbf16b5c4d47a3/5099",
+    img: "./images/4.svg",
+    category: "collectibles",
+  },
+  {
+    id: 5,
+    name: "Scenery Roe",
+    author: "CyberBrokers",
+    price: "2.69",
+    link:
+      "https://opensea.io/assets/ethereum/0x892848074ddea461a15f337250da3ce55580ca85/7614",
+    img: "./images/5.svg",
+    category: "collectibles",
+  },
+  {
+    id: 6,
+    name: "#428 Ollie 'Underrated' Royal LDA",
+    author: "Ollie - Underrated",
+    price: "0.097",
+    link:
+      "https://opensea.io/assets/matic/0x7c885c4bfd179fb59f1056fbea319d579a278075/9868188640707215440437863615521278132652",
+    img: "./images/6.jpg",
+    category: "music",
+  },
+  {
+    id: 7,
+    name: "prouda me. (prod by sango) #25",
+    author: "Iman Europe",
+    price: "0.29",
+    link:
+      "https://opensea.io/assets/ethereum/0x28b708ee88e608f3307a7d41b930312fc9c9e632/1020847100762815390390123822295304634393",
+    img: "./images/7.jpg",
+    category: "music",
+  },
+  {
+    id: 8,
+    name: "kolors #397",
+    author: "monte booker x soulection",
+    price: "0.0198",
+    link:
+      "https://opensea.io/assets/ethereum/0x76cb0647698857d301cb3fe1b6f0e79c03817245/397",
+    img: "./images/8.png",
+    category: "music",
+  },
+  {
+    id: 9,
+    name: "Ace of Cups",
+    author: "Justin Aversano",
+    price: "2",
+    link:
+      "https://opensea.io/assets/ethereum/0x495f947276749ce646f68ac8c248420045cb7b5e/81389452228015721923544690746270190594299388963234071450728086497137442947073",
+    img: "./images/9.jpg",
+    category: "art",
+  },
+  {
+    id: 10,
+    name: "Crypto AI Art #297",
+    author: "MONTREAL.AI",
+    price: "0.1",
+    link:
+      "https://opensea.io/assets/ethereum/0x565abc3feaa3bc3820b83620f4bbf16b5c4d47a3/5099",
+    img: "./images/10.png",
+    category: "art",
+  },
+  {
+    id: 11,
+    name: "Crypto AI Art #184",
+    author: "MONTREAL.AI",
+    price: "180",
+    link:
+      "https://opensea.io/assets/ethereum/0x495f947276749ce646f68ac8c248420045cb7b5e/2392630434290240917728431095880785304289144848761899072947382643889700601857",
+    img: "./images/11.jpg",
+    category: "art",
+  },
+  {
+    id: 12,
+    name: "Bikes of Burden #22. Doors.",
+    author: "Bikes of Burden",
+    price: "1.5",
+    link:
+      "https://opensea.io/assets/ethereum/0x495f947276749ce646f68ac8c248420045cb7b5e/24110165408155573398908987562152031273205884089304227586706331077567987056641",
+    img: "./images/12.jpg",
+    category: "photography",
+  },
+  {
+    id: 13,
+    name: "Edifice #178",
+    author: "Edifice by Ben Kovach",
+    price: "6.9",
+    link:
+      "https://opensea.io/assets/ethereum/0xa7d8d9ef8d8ce8992df33d8b8cf4aebabd5bd270/204000178",
+    img: "./images/13.png",
+    category: "art",
+  },
+  {
+    id: 14,
+    name: "#910",
+    author: "Yaasmyn Fula",
+    price: "0.03",
+    link:
+      "https://opensea.io/assets/ethereum/0x5be372fa04ba4bb290efab27c0d772f9efc0491b/910",
+    img: "./images/14.jpeg",
+    category: "photography",
+  },
+  {
+    id: 15,
+    name: "Mount Mælifell",
+    author: "Powerful Iceland",
+    price: "0.17",
+    link:
+      "https://opensea.io/assets/ethereum/0x495f947276749ce646f68ac8c248420045cb7b5e/71348397378810690825618428912408306798479722983811327082816877900514098937857",
+    img: "./images/15.jpeg",
+    category: "photography",
+  },
+  {
+    id: 16,
+    name: "Cristiano Ronaldo",
+    author: "Wrapped Strikers",
+    price: "100",
+    link:
+      "https://opensea.io/assets/ethereum/0x11739d7bd793543a6e83bd7d8601fcbcde04e798/7310",
+    img: "./images/16.png",
+    category: "sport",
+  },
+  {
+    id: 17,
+    name: "Koray Altınay",
+    author: "Sorare",
+    price: "0.06",
+    link:
+      "https://opensea.io/assets/ethereum/0x629a673a8242c2ac4b7b8c5d8735fbeac21a6205/88493999988572936584082724924671864744433972195785553767281760238569836515097",
+    img: "./images/17.png",
+    category: "sport",
+  },
+  {
+    id: 18,
+    name: "Ink Art #47 LеBrօո",
+    author: "Mesmerizer",
+    price: "0.06",
+    link:
+      "https://opensea.io/assets/matic/0x2953399124f0cbb46d2cbacd8a89cf0599974963/94916783746142258917689322291875902080657954662840061252829770164901269995521",
+    img: "./images/18.jpeg",
+    category: "sport",
+  },
+];
